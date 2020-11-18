769b1c6a3d9c642f05ffcbae81b77c7bfdde3d70
rafetbasturk <62568554+rafetbasturk@users.noreply.github.com>
2020-11-18 04:45:21 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let usedStorage = parseInt(document.getElementById("usage").innerText, 10);
+let totalStorage = 1000;
+let usagePercentage = ((usedStorage / totalStorage) * 100).toFixed(2) + "%" ;
+
+document.getElementById("inside-bar").style.width = usagePercentage;
+
+let leftSpace = 1000 - usedStorage;
+
+document.getElementById("left").firstElementChild.innerText = leftSpace;
